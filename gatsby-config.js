4959452ea3788828085e37178080bf9f0e4d6fd4
Jose M. Perez <JMPerez@users.noreply.github.com>
2019-05-20 07:27:11 +0200

    Improve image quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 1240,
               withWebp: true,
               tracedSVG: true,
               showCaptions: true
