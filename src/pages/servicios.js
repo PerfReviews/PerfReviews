261dea0434f590b7679b64814db8cea6d80c6272
Joan León | @nucliweb <joan.leon@gmail.com>
2019-07-23 08:00:56 +0200

    Added description and price service

--- src/pages/servicios.js
+++ src/pages/servicios.js
@@ -21,40 +21,26 @@ class Servicios extends React.Component {
 
         <p>Tenemos una larga experiencia llevando a cambio mejoras en el rendimiento de webs y estudiando el impacto en las métricas de negocio que te preocupan. Sabemos cuál es el balance entre esfuerzo técnico y retorno de la inversión.</p>
 
-        <h2>Por qué es importante el rendimiento web</h2>
-        <p>La velocidad de carga de una web tiene un gran impacto y es el factor más importante en la experiencia de usuario (fuente: Speed Matters / Designing for Mobile Performance, Awwwards).</p>
-
-        <table style={{'font-size': '0.8em'}}>
-          <tr>
-            <td style={{width: '50%'}}><strong>Velocidad de carga de la página</strong></td>
-            <td style={{width: '50%'}}><div style={{width:"75%", height: '1em', display: 'inline-block', background: '#00AA58', 'margin-right': '1em'}}></div><strong>75%</strong></td>
-          </tr>
-          <tr>
-            <td style={{width: '50%'}}>Facilidad para encontrar lo que busco</td>
-            <td style={{width: '50%'}}><div style={{width:"66%", height: '1em', display: 'inline-block', background: '#eee', 'margin-right': '1em'}}></div>66%</td>
-          </tr>
-          <tr>
-            <td style={{width: '50%'}}>Cómo de bien se adapta la web a mi pantalla</td>
-            <td style={{width: '50%'}}><div style={{width:"61%", height: '1em', display: 'inline-block', background: '#eee', 'margin-right': '1em'}}></div>61%</td>
-          </tr>
-          <tr>
-            <td style={{width: '50%'}}>Facilidad de uso de la web</td>
-            <td style={{width: '50%'}}><div style={{width:"58%", height: '1em', display: 'inline-block', background: '#eee', 'margin-right': '1em'}}></div>58%</td>
-          </tr>
-          <tr>
-            <td style={{width: '50%'}}>Cómo de atractiva es la web</td>
-            <td style={{width: '50%'}}><div style={{width:"24%", height: '1em', display: 'inline-block', background: '#eee', 'margin-right': '1em'}}></div>24%</td>
-          </tr>
-        </table>
-
-        <p>Al incrementar el tiempo de carga también lo hace la tasa de abandono. Cuando el tiempo de carga pasa de 1 segundo a 3 segundos, la probabilidad de que el usuario abandone el sitio aumenta un 32%. Pasando de 1 a 5 segundos aumenta un 90%.</p>
-
-        <p>Las mejoras en rendimiento demuestran estudio tras estudio <strong>mejoras en métricas clave como engagement y facturación</strong>.</p>
-
         <h1>Nuestros servicios</h1>
         <p>Tenemos una larga experiencia en el desarrollo de proyectos web a escala en empresas como Facebook, Spotify, Infojobs, Coches, Tuenti y Mil Anuncios. Con un especial énfasis en usabilidad y rendimiento. Tenemos el reconocmiento de Google como Google Developer Experts en tecnologías web.</p>
+
         <h2>Auditorías de rendimiento web</h2>
-        <p>Realizamos auditorías de rendimiento de sitios web identificando mejoras para reducir tiempos de carga y tráfico de datos.</p>
+        <p>El rendimiento de tu sitio web es muy importante. Una web que carga rápido en cualquier dispositivo y condiciones de red mejora las métricas clave de negocio. Trabajaremos contigo y tu equipo para identificar los principales problemas que afectan a la performance de tu web.</p>
+
+        <p>Una vez realizado el estudio proporcionaremos un documento detallado con consejos para ayudar al equipo de desarrollo a mejorar tanto el rendimiento como la experiencia de usuario de tu web.</p>
+
+        <h4>Nuestro proceso</h4>
+        <ul style={{marginLeft: '2em'}}>
+          <li>Todo empieza con una llamada. Juntos estableceremos las mejoras y objetivos deseados.</li>
+          <li>Proporcionaremos un informe detallado del rendimiento, incluyendo soluciones concretas, listo para pasárselo a tu equipo de desarrollo.</li>
+        <li>Dos reuniones de follow-up de una hora de duración para asegurarnos de que estamos haciendo progresos.</li>
+        </ul>
+
+        <h4>Precio</h4>
+        <p>
+          <span style={{color: 'var(--textColorLink)', fontWeight: 'bold'}}>795 €</span>
+          <i style={{color: '#656565'}}> (IVA no incluido)</i>
+        </p>
 
         <p>¿Has visto <a href="https://www.youtube.com/channel/UCNoF5_1loBFvW2lZXPxp8ww">nuestros vídeos</a> y te gustaría una auditoría de tu web?</p>
         <p><strong><a href="mailto:perfreviews.mail@gmail.com">Contáctanos para condiciones</a>.</strong></p>
