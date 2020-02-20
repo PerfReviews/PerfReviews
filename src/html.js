191a5678c91cd6584495f1d5a3842aeadd4639f4
José M. Pérez <JMPerez@users.noreply.github.com>
2020-02-20 17:17:06 +0100

    Remove FB pixel

--- src/html.js
+++ src/html.js
@@ -24,30 +24,6 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-        <script dangerouslySetInnerHTML={{
-    __html: `
-    (function() {
-      if (location.host === 'perf.reviews') {
-        function loadFacebook() {
-          (function(f,b,e,v,n,t,s) {
-            if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)})(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1251206408378715');
-          fbq('track', 'PageView');
-        }
-
-        if (window.requestIdleCallback) {
-            window.requestIdleCallback(loadFacebook, {timeout: 5000});
-        } else {
-            setTimeout(loadFacebook, 5000);
-        }
-      }
-    })();`}}></script>
       </body>
     </html>
   )
