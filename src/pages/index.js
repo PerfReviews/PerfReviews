68b5a459592a4c3c8c5207a2531e52f5fbd15e07
Joan León <joan.leon@gmail.com>
2024-06-25 16:51:59 +0200

    Remove prices

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,87 +21,202 @@ class Reviews extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="Auditorías de rendimiento web"
-          description="Somos expertos consultores en web performance. Hacemos auditorías de sitios web, workshops y talleres. Identificamos problemas de rendimiento y explicamos herramientas y técnicas para corregirlos y prevenirlos." />
+        <SEO
+          title="Auditorías de rendimiento web"
+          description="Somos expertos consultores en web performance. Hacemos auditorías de sitios web, workshops y talleres. Identificamos problemas de rendimiento y explicamos herramientas y técnicas para corregirlos y prevenirlos."
+        />
 
         <h1>Mejoramos el rendimiento de tu web.</h1>
-        <p>Tenemos una larga experiencia llevando a cambio mejoras en el rendimiento de webs y estudiando el impacto en las métricas de negocio que te preocupan. Sabemos cuál es el balance entre esfuerzo técnico y retorno de la inversión.</p>
-        <p>Hemos desarrollado proyectos web a escala en empresas como Facebook, Spotify, Mediaset, Infojobs, Coches.net, Tuenti y Mil Anuncios. Con un especial énfasis en usabilidad y rendimiento.</p>
-        <p>Tenemos el reconocimiento de Google como <a href="https://developers.google.com/community/experts">Google Developer Experts</a> en tecnologías web y Media Developer Expert por Cloudinary.</p>
-        <p style={{ textTransform: 'uppercase', textAlign: 'center', fontWeight: 'bold', fontSize: '80%', marginBottom: 0, paddingTop: '2em', color: '#9CA3AF' }}>Algunas empresas con las que hemos trabajado</p>
-        <div style={{ display: 'flex', alignItems: "center", flexWrap: 'wrap', paddingBottom: '1.75rem' }}>
-          <div style={{ width: '33%' }}>
-            <img alt="Spotify logo" src={spotifyLogo} style={{ marginBottom: 0, padding: '1em', display: 'block' }} />
+        <p>
+          Tenemos una larga experiencia llevando a cambio mejoras en el
+          rendimiento de webs y estudiando el impacto en las métricas de negocio
+          que te preocupan. Sabemos cuál es el balance entre esfuerzo técnico y
+          retorno de la inversión.
+        </p>
+        <p>
+          Hemos desarrollado proyectos web a escala en empresas como Facebook,
+          Spotify, Mediaset, Infojobs, Coches.net, Tuenti y Mil Anuncios. Con un
+          especial énfasis en usabilidad y rendimiento.
+        </p>
+        <p>
+          Tenemos el reconocimiento de Google como{" "}
+          <a href="https://developers.google.com/community/experts">
+            Google Developer Experts
+          </a>{" "}
+          en tecnologías web y Media Developer Expert por Cloudinary.
+        </p>
+        <p
+          style={{
+            textTransform: "uppercase",
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "80%",
+            marginBottom: 0,
+            paddingTop: "2em",
+            color: "#9CA3AF",
+          }}
+        >
+          Algunas empresas con las que hemos trabajado
+        </p>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "wrap",
+            paddingBottom: "1.75rem",
+          }}
+        >
+          <div style={{ width: "33%" }}>
+            <img
+              alt="Spotify logo"
+              src={spotifyLogo}
+              style={{ marginBottom: 0, padding: "1em", display: "block" }}
+            />
           </div>
-          <div style={{ width: '33%' }}>
-            <img alt="Adevinta logo" src={adevintaLogo} style={{ marginBottom: 0, padding: '1em', display: 'block' }} />
+          <div style={{ width: "33%" }}>
+            <img
+              alt="Adevinta logo"
+              src={adevintaLogo}
+              style={{ marginBottom: 0, padding: "1em", display: "block" }}
+            />
           </div>
-          <div style={{ width: '33%' }}>
-            <img alt="Meta (Facebook) logo" src={metaLogo} style={{ marginBottom: 0, padding: '1em', display: 'block' }} />
+          <div style={{ width: "33%" }}>
+            <img
+              alt="Meta (Facebook) logo"
+              src={metaLogo}
+              style={{ marginBottom: 0, padding: "1em", display: "block" }}
+            />
           </div>
-          <div style={{ width: '33%' }}>
-            <img alt="Kave Home logo" src={kaveHomeLogo} style={{ marginBottom: 0, padding: '1em', display: 'block' }} />
+          <div style={{ width: "33%" }}>
+            <img
+              alt="Kave Home logo"
+              src={kaveHomeLogo}
+              style={{ marginBottom: 0, padding: "1em", display: "block" }}
+            />
           </div>
-          <div style={{ width: '33%' }}>
-            <img alt="Domestika logo" src={domestikaLogo} style={{ marginBottom: 0, padding: '1em', display: 'block' }} />
+          <div style={{ width: "33%" }}>
+            <img
+              alt="Domestika logo"
+              src={domestikaLogo}
+              style={{ marginBottom: 0, padding: "1em", display: "block" }}
+            />
           </div>
-          <div style={{ width: '33%' }}>
-            <img alt="Mediaset logo" src={mediasetLogo} style={{ marginBottom: 0, padding: '1em', display: 'block' }} />
+          <div style={{ width: "33%" }}>
+            <img
+              alt="Mediaset logo"
+              src={mediasetLogo}
+              style={{ marginBottom: 0, padding: "1em", display: "block" }}
+            />
           </div>
         </div>
 
         <h2>Nuestros servicios</h2>
 
         <h3>Auditorías de rendimiento web</h3>
-        <p>El rendimiento de tu sitio web es muy importante. Una web que carga rápido en cualquier dispositivo y condiciones de red mejora las métricas clave de negocio. Trabajaremos contigo y tu equipo para identificar los principales problemas que afectan a la performance de tu web.</p>
+        <p>
+          El rendimiento de tu sitio web es muy importante. Una web que carga
+          rápido en cualquier dispositivo y condiciones de red mejora las
+          métricas clave de negocio. Trabajaremos contigo y tu equipo para
+          identificar los principales problemas que afectan a la performance de
+          tu web.
+        </p>
 
-        <p>Una vez realizado el estudio proporcionaremos un documento detallado con consejos para ayudar al equipo de desarrollo a mejorar tanto el rendimiento como la experiencia de usuario de tu web.</p>
+        <p>
+          Una vez realizado el estudio proporcionaremos un documento detallado
+          con consejos para ayudar al equipo de desarrollo a mejorar tanto el
+          rendimiento como la experiencia de usuario de tu web.
+        </p>
 
         <h4>Nuestro proceso</h4>
-        <ol style={{ marginLeft: '2em' }}>
-          <li>Todo empieza con una llamada. Juntos estableceremos <strong>las mejoras y objetivos deseados</strong>.</li>
-          <li>Proporcionaremos un informe detallado del rendimiento, incluyendo <strong>soluciones concretas</strong>, listo para pasárselo a tu equipo de desarrollo.</li>
-          <li>Planificaremos <strong>2 reuniones de seguimiento</strong> de una hora de duración para asegurarnos de que estamos haciendo progresos.</li>
+        <ol style={{ marginLeft: "2em" }}>
+          <li>
+            Todo empieza con una llamada. Juntos estableceremos{" "}
+            <strong>las mejoras y objetivos deseados</strong>.
+          </li>
+          <li>
+            Proporcionaremos un informe detallado del rendimiento, incluyendo{" "}
+            <strong>soluciones concretas</strong>, listo para pasárselo a tu
+            equipo de desarrollo.
+          </li>
+          <li>
+            Planificaremos <strong>2 reuniones de seguimiento</strong> de una
+            hora de duración para asegurarnos de que estamos haciendo progresos.
+          </li>
         </ol>
 
         <h4>Reserva tu análisis de rendimiento</h4>
-        <p>
-          <span style={{ color: 'var(--textColorLink)', fontWeight: 'bold' }}>795 €</span>
-          <span style={{ color: '#656565' }}> (IVA no incluido)</span>
-        </p>
+        {/* <p> */}
+        {/*   <span style={{ color: "var(--textColorLink)", fontWeight: "bold" }}> */}
+        {/*     795 € */}
+        {/*   </span> */}
+        {/*   <span style={{ color: "#656565" }}> (IVA no incluido)</span> */}
+        {/* </p> */}
 
-        <p style={{ marginBottom: '4em' }}><strong><a style={{ padding: '0.5em 1em', backgroundColor: 'var(--textColorLink)', color: '#fff' }} href="mailto:mail@perf.reviews?subject=Perf Review">Contrata nuestro servicio de análisis</a></strong></p>
+        <p style={{ marginBottom: "4em" }}>
+          <strong>
+            <a
+              style={{
+                padding: "0.5em 1em",
+                backgroundColor: "var(--textColorLink)",
+                color: "#fff",
+              }}
+              href="mailto:mail@perf.reviews?subject=Perf Review"
+            >
+              Contrata nuestro servicio de análisis
+            </a>
+          </strong>
+        </p>
 
         <hr />
 
-        <h3>Consultoría experta</h3>
-        <p>Si tienes dudas concretas sobre tu web o estás planteando mejoras en el rendimiento de la misma, estamos aquí para ayudarte. Contáctanos para reservar una videollamada para que te podamos asesorar.</p>
-        <h4>Reserva tu servicio de consultoría</h4>
-        <p>
-          <span style={{ color: 'var(--textColorLink)', fontWeight: 'bold' }}>99 € / hora</span>
-          <span style={{ color: '#656565' }}> (IVA no incluido)</span>
-        </p>
-
-        <p style={{ marginBottom: '4em' }}><strong><a style={{ padding: '0.5em 1em', backgroundColor: 'var(--textColorLink)', color: '#fff' }} href="mailto:mail@perf.reviews?subject=Consultoría Web Performance">Contrata nuestro servicio de consultoría</a></strong></p>
-
-        <hr />
+        {/* <h3>Consultoría experta</h3> */}
+        {/* <p>Si tienes dudas concretas sobre tu web o estás planteando mejoras en el rendimiento de la misma, estamos aquí para ayudarte. Contáctanos para reservar una videollamada para que te podamos asesorar.</p> */}
+        {/* <h4>Reserva tu servicio de consultoría</h4> */}
+        {/* <p> */}
+        {/*   <span style={{ color: 'var(--textColorLink)', fontWeight: 'bold' }}>99 € / hora</span> */}
+        {/*   <span style={{ color: '#656565' }}> (IVA no incluido)</span> */}
+        {/* </p> */}
+        {/**/}
+        {/* <p style={{ marginBottom: '4em' }}><strong><a style={{ padding: '0.5em 1em', backgroundColor: 'var(--textColorLink)', color: '#fff' }} href="mailto:mail@perf.reviews?subject=Consultoría Web Performance">Contrata nuestro servicio de consultoría</a></strong></p> */}
+        {/**/}
+        {/* <hr /> */}
 
         <h3>Formación: Workshops y talleres</h3>
-        <p>Aprende cuáles son los principales problemas de rendimiento en sitios web, cómo corregirlos y monitorizarlos.</p>
-        <p style={{ marginBottom: '4em' }}><strong><a style={{ padding: '0.5em 1em', backgroundColor: 'var(--textColorLink)', color: '#fff' }} href="mailto:mail@perf.reviews?subject=Información Worlshops">Contáctanos para condiciones</a></strong></p>
+        <p>
+          Aprende cuáles son los principales problemas de rendimiento en sitios
+          web, cómo corregirlos y monitorizarlos.
+        </p>
+        <p style={{ marginBottom: "4em" }}>
+          <strong>
+            <a
+              style={{
+                padding: "0.5em 1em",
+                backgroundColor: "var(--textColorLink)",
+                color: "#fff",
+              }}
+              href="mailto:mail@perf.reviews?subject=Información Worlshops"
+            >
+              Contáctanos para condiciones
+            </a>
+          </strong>
+        </p>
 
         <hr />
 
         <h3>In English</h3>
-        <p><Link to="/en/services/">Read about our services</Link></p>
+        <p>
+          <Link to="/en/services/">Read about our services</Link>
+        </p>
 
-        <hr style={{
-          marginTop: rhythm(2.5),
-          marginBottom: rhythm(2.5),
-        }} />
+        <hr
+          style={{
+            marginTop: rhythm(2.5),
+            marginBottom: rhythm(2.5),
+          }}
+        />
         <h2>Posts recientes en nuestro blog</h2>
         {blog
-          .filter(p => p.node.fields.slug.indexOf("/en/") === -1)
+          .filter((p) => p.node.fields.slug.indexOf("/en/") === -1)
           .map(({ node }) => (
             <PostExtract post={node} />
           ))}
