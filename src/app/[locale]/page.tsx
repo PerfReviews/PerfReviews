0cc891e1645536db0826581c37aeec7cc325e0b9
Andrés Alvarez <a.alvarez.sor@gmail.com>
2024-08-29 21:47:42 +0200

    fix several issues

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -188,7 +188,7 @@ export default function HomePage({ params: { locale } }: HomePageProps) {
               </p>
             </div>
 
-            <div className="grid grid-cols-4">
+            <div className="grid grid-cols-2 md:grid-cols-4">
               {clients.map((name, index) => (
                 <div key={index} className="flex items-center justify-center">
                   <Image
