089103920c8e0f3912182ff650044a9a32772b67
Joan Leon <joan.leon@gmail.com>
2024-09-02 19:51:20 +0200

    Add sw.js to remove the cache

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        }),
+      );
+    }),
+  );
+});
