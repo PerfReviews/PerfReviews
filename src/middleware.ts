6e1cc145a522f89181dbd172094e9d524d1ea1eb
Joan Leon <joan.leon@gmail.com>
2025-12-29 19:49:18 +0100

    fix: exclude /insights from next-intl middleware to allow rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,5 @@ export default function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+  matcher: ["/((?!api|_next|_vercel|insights|.*\\..*).*)"],
 };
